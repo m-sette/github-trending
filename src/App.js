9e6f8e8a09b38b8427b557176cebd962638e6f04
Matte Sette <msette8@gmail.com>
2021-02-22 13:48:58 +0100

    display top 30 trending repos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
 import './App.css'
+import { useEffect, useState } from 'react'
 
-function App() {
+import axios from 'axios'
+
+const App = () => {
+  const [repos, setRepos] = useState([])
+  const [lang, setLang] = useState('')
+
+  async function fetchData() {
+    const res = await axios
+      .get(
+        `https://api.github.com/search/repositories?q=language:${lang}&sort=stars&order=desc`
+      )
+      .then((response) => {
+        setRepos(response.data.items)
+      })
+      .catch((e) => console.log('Error', e))
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  let listRepos = repos.map((repo) => {
+    return (
+      <div className='card' key={repo.id}>
+        <h2>{repo.full_name}</h2>
+        <p>{repo.description}</p>
+      </div>
+    )
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    fetchData()
+  }
+
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <main>
+      <h1>Trending repositories of the week</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          onChange={(e) => setLang(e.target.value)}
+          required
+        ></input>
+        <input type='submit' value='submit'></input>
+      </form>
+
+      {listRepos}
+    </main>
   )
 }
 
